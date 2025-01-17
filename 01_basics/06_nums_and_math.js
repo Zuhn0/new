a7c91343cb091bac3d8473f6c93a6ebb8ba4574d
Zuhn0 <harshsingh0063@gmail.com>
2025-01-17 14:12:31 +0000

    math and nums

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,15 +1,26 @@
-const score= 400
-const balance=new Number(100)
-console.log(balance) //[Number: 100]
+// const score= 400
+// const balance=new Number(100)
+// console.log(balance) //[Number: 100]
 
-console.log(balance.toString().length); //balance now string
-console.log(balance.toFixed(2)); //100.00
+// console.log(balance.toString().length); //balance now string
+// console.log(balance.toFixed(2)); //100.00
 
-const otherNumber=23.8532
-console.log(otherNumber.toPrecision(3)); //23.9
+// const otherNumber=23.8532
+// console.log(otherNumber.toPrecision(3)); //23.9
 
-const hundreds=1000000
-console.log(hundreds.toLocaleString('en-IN')); //10,00,000
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN')); //10,00,000
+
+// // +++++++++++++++++++++Maths+++++++++++++++++
+// console.log(Math); //Object [Math] {}
+
+// console.log(Math.ceil(4.3));
+// console.log(Math.max(4,3,6,8)); //8
+// console.log(Math.min(4,5,2,6));  //2
+//  console.log(Math.round(Math.random()*10));
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min);
 
 
 
