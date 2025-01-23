ca8db1167f8b004152e8c05751b10f86155a5d47
Zuhn0 <harshsingh0063@gmail.com>
2025-01-23 12:06:47 +0000

    date and time

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -11,6 +11,20 @@ let myCreatedDate=new Date("2025-01-23")
 // console.log(myCreatedDate.toString()); //Thu Jan 23 2025 00:00:00 GMT+0000
 // console.log(myCreatedDate.toLocaleDateString()); //1/23/2025
 // console.log(myCreatedDate.toLocaleString()); //1/23/2025, 12:00:00 AM
+let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(myTimeStamp/1000)); //toget in seconds
+
+let newDate = new Date()
+// console.log(newDate.getMonth()+1);
+
+// console.log(`${newDate.getDay()} and the time is ${newDate.getTime()}`);
+
+newDate.toLocaleString( 'default' ,{
+   weekday:"long",
+})
+
 
 
 
