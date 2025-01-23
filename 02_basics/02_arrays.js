b26fde7ad10c9c69b35630507da2f5a4b107b6ec
Zuhn0 <harshsingh0063@gmail.com>
2025-01-23 13:35:52 +0000

    ARRAY2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,5 @@
+ const marvel_heros=["thor","spiderman","Ironman"]
+const dc=["superman","flash","batman"]
+
+marvel_heros.push(dc);
+console.log(marvel_heros); //[ 'thor', 'spiderman', 'Ironman', [ 'superman', 'flash', 'batman' ] ]
