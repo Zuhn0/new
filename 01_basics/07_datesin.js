affd0a3abff0c37c0e8bcc9ae06d82f63864961e
Zuhn0 <harshsingh0063@gmail.com>
2025-01-23 11:41:34 +0000

    date and time

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,16 @@
+//DATES
+let myDate=new Date()
+// console.log(myDate.toUTCString()); //Thu, 23 Jan 2025 11:22:16 GMT
+// console.log(myDate.toDateString()); //Thu Jan 23 2025
+// console.log(myDate.toLocaleDateString()); //1/23/2025
+// console.log(typeof myDate) //object
+
+// let myCreatedDate=new Date(2025,0,25,5,3) //month start from 0 in js
+// console.log(myCreatedDate);
+let myCreatedDate=new Date("2025-01-23")
+// console.log(myCreatedDate.toString()); //Thu Jan 23 2025 00:00:00 GMT+0000
+// console.log(myCreatedDate.toLocaleDateString()); //1/23/2025
+// console.log(myCreatedDate.toLocaleString()); //1/23/2025, 12:00:00 AM
+
+
+
