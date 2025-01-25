3d31d0217cf20f3d09e7db5766c8ccf073decaf3
Zuhn0 <harshsingh0063@gmail.com>
2025-01-25 10:48:03 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,8 +1,44 @@
 // const tinderUser = new Object(); //singleton object
 
-const tinderUser ={}; // non singelton object
+// const tinderUser ={}; // non singelton object
+// tinderUser.id="123abc"
+// tinderUser.name="Aakash"
 
+// // console.log(tinderUser); //{ id: '123abc', name: 'Aakash' }
 
-console.log(tinderUser);
+// const regularUser = {
+//      email: "harshsingh0063@gmail.com",
+//      fullname: {
+//         userfullname: {
+//             firstname: "Aakash",
+//             lastname: "singh"
+//         }
+//      }
+// }
+// console.log(regularUser.fullname); //{ userfullname: { firstname: 'Aakash', lastname: 'singh' } }
+// console.log(regularUser.fullname.userfullname.lastname); //singh
 
+const obj1={
+    1:"a",
+    2:"b"
+}
+const obj2={
+    3:"a",
+    4:"b"
+}
+const obj3={
+    5:"a",
+    6:"b"
+}
 
+// const obj3={obj1 , 
+//     obj2}
+
+// console.log(obj3); //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+// const obj4=Object.assign({},obj1,obj2,obj3) 
+// console.log(obj4); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const obj4={...obj1,...obj2,...obj3}
+
+console.log(obj4) //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
