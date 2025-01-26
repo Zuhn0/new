5c5b3ebc84a9eefec0a060be963a83434427420a
Zuhn0 <harshsingh0063@gmail.com>
2025-01-26 09:00:43 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+//Function
+
+function sayMyName()
+{
+    console.log("A");
+    console.log("A");
+    console.log("K");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+    
+}
+// sayMyName()  
+// /*A
+// A
+// K
+// A
+// S
+// H */
+
+// function addTwoNum(number1,number2)
+// {
+//    console.log(number1+number2);
+// }
+
+// // addTwoNum(3,"a") //3a
+// // addTwoNum(3,null) //3
+// const result=addTwoNum(3,4)  //7
+// console.log("Result: ", result); //Result:  undefined
+
+function addTwoNum(number1,number2)
+{
+  return (number1+number2);
+}
+const result=addTwoNum(3,4)
+console.log("result: " , result);  //result:  7
