79d319a11eb3ec6e3f88b00b91ebb580afad6d99
Zuhn0 <harshsingh0063@gmail.com>
2025-02-01 07:53:19 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+// 'if' same as c++
+
+
+if(2=="2") //double equal
+{console.log("YES");}
+else{ console.log("NO");}   //YES
+
+
+if(2==="2") //triple equal 
+    {console.log("YES");}
+    else{ console.log("NO");}   //NO
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,11 @@
+const month=3
+switch(month)
+{
+    case 3:
+         console.log("before April");
+    break;
+    default: 
+    console.log("no");
+    break;
+}
+
