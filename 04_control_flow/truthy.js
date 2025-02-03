07da78e68d9b68ba950ef61cc36880263f9b6a54
Zuhn0 <harshsingh0063@gmail.com>
2025-02-03 19:34:00 +0000

    control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,32 +1,63 @@
-const userEmail="h@hitesh.ai"
+// const userEmail="h@hitesh.ai"
 
-if(userEmail){
-    console.log("Got user email");}
-    else{
-       console.log("dont");
+// if(userEmail){
+//     console.log("Got user email");}
+//     else{
+//        console.log("dont");
        
-    }
-     //Got user email
-    const useerEmail=""
+//     }
+//      //Got user email
+//     const useerEmail=""
 
-if(useerEmail){
-    console.log("Got user email");}
-    else{
-       console.log("dont");
+// if(useerEmail){
+//     console.log("Got user email");}
+//     else{
+//        console.log("dont");
        
-    }   //dont
+//     }   //dont
    
-//FALSY VALUES
+// //FALSY VALUES
 
-// false,0,-0,BigInt 0n,"",null,undefined,NaN
+// // false,0,-0,BigInt 0n,"",null,undefined,NaN
 
 
-//truthy VALUES
+// //truthy VALUES
 
-// "0",'false'," ",[],{},function(){}
+// // "0",'false'," ",[],{},function(){}
 
- const emptyObj={}
+//  const emptyObj={}
 
- if(Object.keys(emptyObj).length===0){
-    console.log("Object is empty");   
- }    //Object is empty
+//  if(Object.keys(emptyObj).length===0){
+//     console.log("Object is empty");   
+//  }    //Object is empty
+
+ //Nullish Coalecsing Operator (??):null undefined
+
+//  let val1;
+//  val1=5 ?? 10
+
+//  console.log(val1); //5
+
+// let val1;
+// val1=null ?? 10
+
+// console.log(val1); //10
+ 
+// let val1;
+// val1=undefined ?? 15
+
+// console.log(val1); //15
+
+let val1;
+val1=undefined ?? 15 ?? 35 //1st value gets assigned
+
+console.log(val1); //15
+
+//TERNIARY OPERATOR 
+
+// condition? true : false
+
+const iceTeaPrice =100 
+iceTeaPrice>100 ? console.log("true") : console.log("false"); //false
+
+
