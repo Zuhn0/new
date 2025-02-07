fd442dfe0c3547fcf40684bf6d88a6e541c99c46
Zuhn0 <harshsingh0063@gmail.com>
2025-02-07 18:51:28 +0000

    filter map

---
+++ 05_iterations/six.js
@@ -0,0 +1,13 @@
+//////////////FILTER MAP////////////
+
+
+// const coding=["js", "ruby", "java", "python","cpp"]
+
+// const values=coding.forEach(item=>{
+//     console.log(item);
+// })
+// console.log(values); //undefined
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+my
