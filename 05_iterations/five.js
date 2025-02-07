0491fc5d4977bc806472a900143a9a4bac598754
Zuhn0 <harshsingh0063@gmail.com>
2025-02-07 18:39:49 +0000

    high order array loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,32 +1,65 @@
 const coding=["js", "ruby", "java", "python","cpp"]
 
-//callback//
-//automatically gets inside coding items one by one
+// //callback//
+// //automatically gets inside coding items one by one
 
-// coding.forEach( function (item){
-//    console.log(item);
-// }  )
-// // js
-// // ruby
-// // java
-// // python
-// // cpp
+// // coding.forEach( function (item){
+// //    console.log(item);
+// // }  )
+// // // js
+// // // ruby
+// // // java
+// // // python
+// // // cpp
 
-// coding.forEach((item)=>{
+// // coding.forEach((item)=>{
+// //     console.log(item);
+// // })
+// // // js
+// // // ruby
+// // // java
+// // // python
+// // // cpp
+
+// function printme(item){
 //     console.log(item);
-// })
+// }
+// coding.forEach(printme);
 // // js
 // // ruby
 // // java
 // // python
 // // cpp
 
-function printme(item){
-    console.log(item);
-}
-coding.forEach(printme);
-// js
-// ruby
-// java
-// python
-// cpp
+// coding.forEach((item,index,arr)=>{
+// console.log(item,index,arr);
+// }
+// )
+// // js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// // ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// // java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// // python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// // cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+
+// const mycoding=[
+//     {
+//       languageName:"Javascript",
+//       languageFileName:"js"
+//     },
+//     {
+//         languageName:"c++",
+//         languageFileName:"cpp"
+//       },
+//       {
+//         languageName:"html",
+//         languageFileName:"html"
+//       }
+// ]
+// mycoding.forEach(item=>{
+
+//     console.log(item.languageName);
+// }
+// )
+// // Javascript
+// // c++
+// // html
