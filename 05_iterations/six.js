c87090438810b1f1ecd642266ef0d8fb38702a19
Zuhn0 <harshsingh0063@gmail.com>
2025-02-07 19:39:14 +0000

    map filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -10,4 +10,35 @@
 
 const myNums=[1,2,3,4,5,6,7,8,9,10]
 
-my
+// const newNums=myNums.filter((num)=>num>4)
+
+// console.log(newNums);
+// //[ 5, 6, 7, 8, 9, 10 ]
+
+// const nnewNums=myNums.filter((num)=>
+//     {num>4
+//     })
+
+// console.log(nnewNums);
+// //[]   //we have to write return statement as we have started a scope
+
+// const nnewwNums=myNums.filter((num)=>
+//     { return num>4
+//     })
+
+// console.log(nnewwNums); 
+// //[ 5, 6, 7, 8, 9, 10 ]
+
+//BY USING FOR EACH
+
+const newNums=[]
+
+myNums.forEach((num)=>{
+    if(num>4)
+    {
+        newNums.push(num)
+    }
+}
+)
+console.log(newNums); 
+//[ 5, 6, 7, 8, 9, 10 ]
